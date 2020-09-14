fd192a8435bc52f14e7dbed4512e6a34f39774c7
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-09-14 09:14:32 -0400

    Départ

--- src/App.js
+++ src/App.js
@@ -7,45 +7,36 @@ export default class App {
 	 * dans leur propriété de classe correspondante.
 	 */
 	static demanderInfos() {
+		// CODE TEMPORAIRE ##########################################
+		this.caractere = "*";
+		this.taille = 5;
+		//###########################################################
+
 		//===========================================================
 		// Demander le caractère à l'usager
 		//===========================================================
-		this.caractere = prompt("Donnez un caractère", "*");
-		// Si le caractère est null (l'usager a fait annuler), on utilise "*"
-		if (this.caractere === null) {
-			this.caractere = "*"
-		}
-		// Si la chaine a plus de 1 caractère, on ne garde que le premier
-		if (this.caractere.length > 1) {
-			this.caractere = this.caractere[0];
-		}
-		// Si le caractère est l'espace ou si la chaine est vide, on utilise "*"
-		if (this.caractere === "" || this.caractere === " ") {
-			this.caractere = "*"
-		}
 
+		// Si le caractère est null (l'usager a fait annuler), on utilise "*"
+
+		// Si la chaine a plus de 1 caractère, on ne garde que le premier
+
+		// Si le caractère est l'espace ou si la chaine est vide, on utilise "*"
+
+		
 		//===========================================================
 		// Demander la taille à l'usager
 		//===========================================================
-		this.taille = prompt("Quelle taille?", 5);
+
 		// Si la taille est null (l'usager a fait annuler), on utilise "5"
-		if (this.taille === null) {
-			this.taille = "5"
-		}
+
 		// La taille doit être transformée en entier
-		this.taille = parseInt(this.taille);
+
 		// Si la taille donnée n'était pas un nombre (NaN), on utilise 5 
-		if (isNaN(this.taille)) {
-			this.taille = 5;
-		}
+
 		// Si la taille est plus grande que 15, on utilise 15
-		if (this.taille > 15) {
-			this.taille = 15;
-		}
+
 		// Si la taille est plus petite que 3, on utilise 3
-		if (this.taille < 3) {
-			this.taille = 3;
-		}
+
 	}
 	static rangee() {
 		//	<div class="tableau">
@@ -54,12 +45,6 @@ export default class App {
 		var app = document.getElementById("app");
 		var tableau = app.appendChild(document.createElement("div"));
 		tableau.classList.add("tableau");
-		var ligne = tableau.appendChild(document.createElement("div"));
-		var html = "";
-		for (let i = 0; i < this.taille; i += 1) {
-			html += this.caractere;
-		}
-		ligne.innerHTML = html;
 	}
 	static colonne() {
 		//	<div class="tableau">
@@ -72,12 +57,6 @@ export default class App {
 		var app = document.getElementById("app");
 		var tableau = app.appendChild(document.createElement("div"));
 		tableau.classList.add("tableau");
-		for (let i = 0; i < this.taille; i += 1) {
-			var ligne = tableau.appendChild(document.createElement("div"));
-			var html = "";
-			html += this.caractere;
-			ligne.innerHTML = html;
-		}
 	}
 	static carre() {
 		//	<div class="tableau">
@@ -90,14 +69,6 @@ export default class App {
 		var app = document.getElementById("app");
 		var tableau = app.appendChild(document.createElement("div"));
 		tableau.classList.add("tableau");
-		for (let j = 0; j < this.taille; j += 1) {
-			var ligne = tableau.appendChild(document.createElement("div"));
-			var html = "";
-			for (let i = 0; i < this.taille; i += 1) {
-				html += this.caractere;
-			}
-			ligne.innerHTML = html;
-		}
 	}
 	static triangle() {
 		//	<div class="tableau">
@@ -110,14 +81,6 @@ export default class App {
 		var app = document.getElementById("app");
 		var tableau = app.appendChild(document.createElement("div"));
 		tableau.classList.add("tableau");
-		for (let j = 0; j < this.taille; j += 1) {
-			var ligne = tableau.appendChild(document.createElement("div"));
-			var html = "";
-			for (let i = 0; i <= j; i += 1) {
-				html += this.caractere;
-			}
-			ligne.innerHTML = html;
-		}
 	}
 	static carreVide() {
 		//	<div class="tableau">
@@ -130,28 +93,6 @@ export default class App {
 		var app = document.getElementById("app");
 		var tableau = app.appendChild(document.createElement("div"));
 		tableau.classList.add("tableau");
-		var ligne = tableau.appendChild(document.createElement("div"));
-		var html = "";
-		for (let i = 0; i < this.taille; i += 1) {
-			html += this.caractere;
-		}
-		ligne.innerHTML = html;
-		for (let j = 0; j < this.taille - 2; j += 1) {
-			var ligne = tableau.appendChild(document.createElement("div"));
-			var html = "";
-			html += this.caractere
-			for (let i = 0; i < this.taille - 2; i += 1) {
-				html += " ";
-			}
-			html += this.caractere
-			ligne.innerHTML = html;
-		}
-		var ligne = tableau.appendChild(document.createElement("div"));
-		var html = "";
-		for (let i = 0; i < this.taille; i += 1) {
-			html += this.caractere;
-		}
-		ligne.innerHTML = html;
 	}
 	static triangleVide() {
 		//	<div class="tableau">
@@ -164,29 +105,6 @@ export default class App {
 		var app = document.getElementById("app");
 		var tableau = app.appendChild(document.createElement("div"));
 		tableau.classList.add("tableau");
-		var ligne = tableau.appendChild(document.createElement("div"));
-		var html = "";
-		html += this.caractere;
-		ligne.innerHTML = html;
-		for (let j = 0; j < this.taille - 2; j += 1) {
-			var ligne = tableau.appendChild(document.createElement("div"));
-			var html = "";
-			html += this.caractere
-			for (let i = 0; i < j; i += 1) {
-				html += " ";
-			}
-			html += this.caractere
-			ligne.innerHTML = html;
-		}
-		var ligne = tableau.appendChild(document.createElement("div"));
-		var html = "";
-		for (let i = 0; i < this.taille; i += 1) {
-			html += this.caractere;
-		}
-		ligne.innerHTML = html;
-	}
-	static main() {
-		var app = document.getElementById("app");
 	}
 	/**
 	 * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
